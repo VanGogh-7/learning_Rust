1abde7dce96e474bb3928295ca6adeb513483914
van-gogh <weiqifan0524@gmail.com>
2026-05-24 23:01:22 +0800

    day4.0

---
+++ Chapter_5/src/main.rs
@@ -0,0 +1,33 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+struct Colour(i32, i32, i32);
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count, //..user1  this set user2 have the same value.
+    };
+}
+
+
+fn build_user(email:String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
