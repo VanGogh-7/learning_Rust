400c7db56c1087fd0b0252119f549d34fcf4041b
van-gogh <weiqifan0524@gmail.com>
2026-05-28 10:35:45 +0800

    day6.0

---
+++ Chapter_7/restaurant/src/lib.rs
@@ -0,0 +1,34 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() { }
+    mod back_of_house {
+        fn fix_incorrect_order() {
+            cook_order();
+            super::deliver_order();
+        }
+        fn cook_order() {}
+        pub enum Appetizer {
+            Soup,
+            Salad,
+        }
+    }
+
+pub fn eat_at_restaurant2() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
